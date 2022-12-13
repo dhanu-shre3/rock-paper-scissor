3d44956c5dd6f410e740d7f1ca314ac53e1da5a6
Dhanushree M Somaiah <dhanushreemsomaiah@Dhanus-MacBook-Air.local>
2022-12-13 12:12:54 +0000

    javascript changes to make the game interactive

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 //cashing a dom
-const userScore = 0;
-const computerScore = 0;
+let userScore = 0;
+let computerScore = 0;
 const userScore_span = document.getElementById("user-score");
 //he uses _span or _div to diffrentiate between span variables and other variables
 const computerScore_span = document.getElementById("comp-score");
 const scoreBoard_div = document.querySelector(".score-board"); //.score-board - because its a class
-const result_div = document.querySelector(".result");
+const result_p = document.querySelector(".result > p"); //get the result in the paragraph tag
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 
@@ -22,25 +22,59 @@ function getComputerChoice(){
     return choices[randomNumber]
 }
 
+function converToWord(letter){
+    if (letter === "r") return "Rock";
+    if (letter === "p") return "Paper";
+    return "Scissors";
+}
+function win(userChoice, computerChoice){
+  userScore++;
+  //getting into the span tag
+  userScore_span.innerHTML = userScore;
+  computerScore_span.innerHTML = computerScore;
+  //const smallUserWord = "user".fontsize(3).sup();
+//   result_p.innerHTML = converToWord(userChoice) + "beats" + converToWord(computerChoice) + "You Win!" 
+//ES6
+   result_p.innerHTML = `${converToWord(userChoice)}(user) beats ${converToWord(computerChoice)}(comp). You Win!`;
+
+}
+
+function lose(userChoice, computerChoice){
+    computerScore++;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    result_p.innerHTML = `${converToWord(userChoice)}(user) loses to ${converToWord(computerChoice)}(comp). You Lost:(`;
+    
+}
+
+function draw(userChoice, computerChoice){
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    result_p.innerHTML = `${converToWord(userChoice)}(user) equals ${converToWord(computerChoice)}(comp). its a draw :|`;
+      
+}
 function game(userChoice){
     const computerChoice = getComputerChoice();
     switch (userChoice + computerChoice) {
         case "rs":
         case "pr":
         case "sp":
-            console.log("USER WINS.");
+            // console.log("USER WINS.");
+            win(userChoice, computerChoice)//function when the user wins
             break;
         case "rp":
         case "ps":
         case "sr":
-            console.log("USER LOSES.");
+            // console.log("USER LOSES.");
+            lose(userChoice, computerChoice)
             break;
         case "rr":
         case "pp":
         case "ss":
-            console.log("its a draw.");
+            // console.log("its a draw.");
+            draw(userChoice, computerChoice)
             break;
-                                        
+
     }
 }
 
